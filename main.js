f0eab75981523802496877dbefe12b4f64d92c48
AhmedTitu <ahmedibnahasan@gmail.com>
2021-04-10 00:20:18 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+//selector
+
+const filterInput = document.querySelector('#filter');
+const msg = document.querySelector('.msg');
+const productListUl = document.querySelector('.collection');
+const nameInput = document.querySelector('.product-name');
+const priceInput = document.querySelector('.product-price');
+const addBtn = document.querySelector('.add-product')
+const deleteBtn = document.querySelector('.delete-product')
+
+
+
